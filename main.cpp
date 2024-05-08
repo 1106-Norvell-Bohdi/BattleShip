62df2b2979cd2366af73e49f7029e3935d9f7656
Keegan Evans <keegane@nevada.unr.edu>
2024-05-07 17:36:03 -0700

    Added to main

--- main.cpp
+++ main.cpp
@@ -5,5 +5,18 @@
 #include "helpers.h"
 
 int main(){
+    string choice;
+    Board player_atk, player_def, AI_atk, AI_def;
+    User player; 
+    User AI;
+    system("clear");
     
+    cout<<"Welcome to Battle Ship"<<endl;
+    cout<<"======================"<<endl;
+    cout<<"Would you like to play? (Y/N): ";
+    cin>>choice;
+        
+    if(choice == "n" || choice == "N"){
+        return 0;
+    }
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 battleship: main.o helpers.o ship.o cell.o board.o user.o
-	g++ -o battlship main.o helpers.o ship.o cell.o board.o user.o -g
+	g++ -o battleship main.o helpers.o ship.o cell.o board.o user.o -g
 
 main.o: helpers.h ship.h cell.h board.h user.h main.cpp
 	g++ -c main.cpp -g
